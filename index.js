c4b78140f58b6bd8dfa09375e8791ae5eecb2cd2
= <=>
2021-08-12 14:17:28 -0300

    Inserindo as operações de Create, Read, Update e Delete

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-(async ()=>{
+(async () => {
     const database = require("./config/database")
-    const Produto = require("./models/produtos")
-    try{
+    const Produto = require("./src/models/produtos")
+    try {
         const resultado = await database.sync()
-        const resultadoCreate = await Produto.create({
+        /* const resultadoCreate = await Produto.create({
             nome: "mouse",
             preco: 10,
             descricao: "Um mouse USB bonitão"
-        })
-        console.log(resultadoCreate)
-
-    }catch(error){
+        }) 
+        console.log(resultadoCreate)*/
+        const produtos = await Produto.findAll()
+        /*  const prod = await Produto.findByPk(9)
+        prod.nome = "Dente de Ouro !"
+        prod.save()
+        console.log(prod) */
+        Produto.destroy({ where: { id: 9 } })
+        console.log(produtos)
+    } catch (error) {
         console.log(error)
     }
 })
-()
+    ()
