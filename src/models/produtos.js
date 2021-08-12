f8cbbe3a89591b3a3d6c1300e25adcccb2495d30
= <=>
2021-08-12 14:17:12 -0300

    Colocando o model de Produto na pasta src

---
+++ src/models/produtos.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize")
+const database = require("../../config/database")
+
+const Produto = database.define('produtos',{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    preco:{
+        type: Sequelize.FLOAT,
+        allowNull: false
+    },
+    descricao:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+    
+});
+
+module.exports = Produto
