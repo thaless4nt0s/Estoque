562708ed9ac1c003455503a9c7de0255563ce794
= <=>
2021-08-12 14:39:41 -0300

    Removendo o CRUD do index e colocando no ./routes/produtos

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-(async () => {
-    const database = require("./config/database")
-    const Produto = require("./src/models/produtos")
-    try {
-        const resultado = await database.sync()
-        /* const resultadoCreate = await Produto.create({
-            nome: "mouse",
-            preco: 10,
-            descricao: "Um mouse USB bonitão"
-        }) 
-        console.log(resultadoCreate)*/
-        const produtos = await Produto.findAll()
-        /*  const prod = await Produto.findByPk(9)
-        prod.nome = "Dente de Ouro !"
-        prod.save()
-        console.log(prod) */
-        Produto.destroy({ where: { id: 9 } })
-        console.log(produtos)
-    } catch (error) {
-        console.log(error)
-    }
+const express = require("express")
+const app = express()
+const produtoRouter = require("./src/routes/produtos")
+require("./config/database")
+
+app.use("/produtos", produtoRouter)
+
+app.listen(3000, () => {
+    console.log('Conectado com sucesso')
 })
-    ()
