b5efff8ea5ea2c37f4690e69e606187e7eaa8794
= <=>
2021-08-12 15:01:58 -0300

    Criando a rota do post

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -3,12 +3,27 @@ const router = express.Router()
 const database = require("../../config/database")
 const Produto = require("../models/produtos")
 
-router.get("/", async (req, res)=>{
-    try{
+router.get("/", async (req, res) => {
+    try {
         await database.sync()
         const resultado = await Produto.findAll()
         res.status(200).json(resultado)
-    }catch(err){
+    } catch (err) {
+        res.status(402).json(err)
+    }
+})
+
+router.post("/", async (req, res) => {
+    let { nome, preco, descricao } = req.body
+    try {
+        await database.sync()
+        const create = Produto.create({
+            nome: nome,
+            preco: preco,
+            descricao: descricao
+        })
+        res.status(200).json(create)
+    } catch (err) {
         res.status(402).json(err)
     }
 })
